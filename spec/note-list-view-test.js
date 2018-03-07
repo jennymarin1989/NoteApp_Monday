85fe715c90a32b8635d89334b01b9f9db002d41b
Lewis Watson <lewwwiswatson@gmail.com>
2018-03-07 14:32:47 +0000

    Add and test createView to build HTML string

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,14 +1,16 @@
 var note1 = new Note("My first Note");
 var note2 = new Note("My second Note");
 
-listViewCreatesView();
+
 
 function listViewCreatesView(){
-  // var list = new List();
-  var listview = new ListView(new List());
+  var list = new List();
+  var listview = new ListView(list);
   describe("create view");
   it("returns a html of the note list");
-  list.addNoteToList("My first Note");
-  // list.addNoteToList("My second Note");
+  list.addNoteToList("My first note");
+  list.addNoteToList("My second note");
   expect.isEqual(listview.createView(), "<ul><li><div>My first note</div></li><li><div>My second note</div></li></ul>");
 };
+
+listViewCreatesView();
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,16 +1,18 @@
 (function(exports){
 
   function ListView(notelist){
-    this.view = this.createView();
     this.notelist = notelist;
+    this.view = this.createView();
   }
 
   ListView.prototype.createView = function() {
     var string = "<ul>";
-    console.log(this.notelist);
     var listArray = this.notelist.getAllNotes();
-
-
+    for (var i = 0; i < listArray.length; i++) {
+      string += `<li><div>${listArray[i].returnText()}</div></li>`;
+    }
+    string += "</ul>";
+    return string;
   };
 
   exports.ListView = ListView;
