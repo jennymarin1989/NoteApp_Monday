1695ebe501a590cef72f7edd57515856f8d5f284
jose <jennymarin1989@hotmail.com>
2018-03-08 16:55:30 +0000

    Creating a note list view model

--- public/note-controller.js
+++ public/note-controller.js
@@ -1,1 +1,19 @@
-document.getElementById('app').innerHTML = "howdy";
+(function (exports){
+
+  function NoteController(notelist){
+    this.notelist = notelist;
+    this.note = notelist.addNoteToList("Favourite drink: Mojito");
+    this.view = new ListView(notelist);
+  }
+
+  NoteController.prototype.getHtml = function(){
+    document.getElementById('app').innerHTML = this.view.createView();
+  };
+
+  exports.NoteController = NoteController;
+
+})(this);
+
+var notelist = new List();
+var notecontroller = new NoteController(notelist);
+notecontroller.getHtml();
